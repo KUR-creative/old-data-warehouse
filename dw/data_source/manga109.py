adf62ba79f1831e7ecc5f0519e4772fed3ac02ea
kur <rhdnfka94@gmail.com>
2020-04-22 13:31:34 +0900

    Fix manga109 dir structure check and improve error reporting

--- dw/data_source/manga109.py
+++ dw/data_source/manga109.py
@@ -27,10 +27,8 @@ def is_valid(root):
     rdir = Path(root)
     idir = Path(root, 'images')
     adir = Path(root, 'manga109-annotations')
-    if((not rdir.exists()) and
-       (not idir.exists()) and
-       (not adir.exists())):
-        print('Manga109 dset dir structure is invalid')
+    if not (rdir.exists() and idir.exists() and adir.exists()):
+        print('Manga109 root directory structure is invalid')
         return False
     
     file_name = lambda p: Path(p).stem
@@ -38,8 +36,15 @@ def is_valid(root):
     xml_names = fp.lmap(file_name, fu.human_sorted(fu.children(adir)))
     
     eq_len = (len(titles) == len(xml_names)) 
+    if not eq_len:
+        print('No title or annotation')
+        return False
+    
     eq_names = bool(sum(
         map(fp.equal, titles, xml_names)
     ))
+    if not eq_names:
+        print('Some xml names are mismatched')
+        return False
     
-    return eq_len and eq_names
+    return True
