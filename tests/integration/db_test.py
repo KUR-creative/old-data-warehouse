f9c889577e7ce547b586b0ed954e586652803d8c
kur <rhdnfka94@gmail.com>
2020-06-10 23:35:26 +0900

    Test: Add old_snet integration test[WIP]

--- tests/integration/db_test.py
+++ tests/integration/db_test.py
@@ -4,7 +4,7 @@ from parse import parse
 import pytest
 
 from dw.data_source import old_snet
-from dw.log import get_cli_cmds
+from dw import db
 
 #@pytest.fixture()
 def test_old_snet_add_data(root, conn):
@@ -16,18 +16,28 @@ def test_old_snet_add_data(root, conn):
     if not db_parsed:
         assert False, f'Invalid connection string:\n{conn}'
         
-    if root is None:
-        # It is just for convenient testing. It could be crashed.
-        #root_parsed = parse('pythona main.py add old_snet {} <connection>',
-        root_parsed = parse('python main.py add old_snet {} <connection>',
-              get_cli_cmds(db_parsed, False)[1]['command'])
-        root = root_parsed[0] if root_parsed else None
     if root is None or (not Path(root).exists()):
         assert False, f'Invalid root: {root}'
 
     print(root)
     print(conn)
+    
+    # REINIT
+    db.run(db.DROP_ALL_QUERY, *db_parsed)
+    schema = './dw/schema/szmc_0.1.0.sql' # TODO: use latest schema file finder(#39)
+    with open(schema, 'r') as s:
+        db.init(s.read(), *db_parsed)
+        
+    # Add 
+    result = old_snet.add_data(root, db_parsed)
+    assert result is None
+
+    # Check properties of DB: What props to check? Think it!
+    num_imgs = len(list(Path(root, 'image').glob('*')))
+    print(len(num_imgs))
     assert False
-    #result = old_snet.add_data(root, db_parsed)
+    # row-count(file) = 3 * count(img in snet285/image): img, rbk, wk
+    # no row in dataset
+    # 2 row in executed_command
     
 #def test_(conn): assert conn == 'db'
