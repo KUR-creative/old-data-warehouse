2803139d30eee5a8a03c9dadd9a75e4db0403e19
kur <rhdnfka94@gmail.com>
2020-06-12 16:15:13 +0900

    Test: Add db test for old_snet.add_data

--- tests/integration/db_test.py
+++ tests/integration/db_test.py
@@ -1,28 +1,26 @@
 from pathlib import Path
 
+from pypika import Table
+from pypika import functions as fn
 from parse import parse
 import pytest
 
 from dw.data_source import old_snet
 from dw import db
 
-#@pytest.fixture()
+
 def test_old_snet_add_data(root, conn):
+    #### GIVEN #####################################################
     if not conn:
         pytest.skip('this tests old_snet.add_data(root, conn)')
-        
-    # TODO: check integrity of connection string(with refactoring cli)
-    db_parsed = parse('{}:{}@{}:{}/{}', conn)
+    db_parsed = parse('{}:{}@{}:{}/{}', conn) # TODO: check integrity of connection string(with refactoring cli)
     if not db_parsed:
         assert False, f'Invalid connection string:\n{conn}'
-        
     if root is None or (not Path(root).exists()):
         assert False, f'Invalid root: {root}'
 
-    print(root)
-    print(conn)
-    
-    # REINIT
+    #### WHEN #####################################################
+    # Reinit
     db.run(db.DROP_ALL_QUERY, *db_parsed)
     schema = './dw/schema/szmc_0.1.0.sql' # TODO: use latest schema file finder(#39)
     with open(schema, 'r') as s:
@@ -32,12 +30,21 @@ def test_old_snet_add_data(root, conn):
     result = old_snet.add_data(root, db_parsed)
     assert result is None
 
-    # Check properties of DB: What props to check? Think it!
+    #### THEN #####################################################
+    # Check properties of DB
     num_imgs = len(list(Path(root, 'image').glob('*')))
-    print(len(num_imgs))
-    assert False
-    # row-count(file) = 3 * count(img in snet285/image): img, rbk, wk
-    # no row in dataset
-    # 2 row in executed_command
     
-#def test_(conn): assert conn == 'db'
+    num_files = db.get(
+        Table('file').select(fn.Count('*')), *db_parsed
+    )[0]['count']
+    assert num_files == 3 * num_imgs, 'DB has img, wk, rbk images: 3 * num_img'
+    
+    num_masks = db.get(
+        Table('mask').select(fn.Count('*')), *db_parsed
+    )[0]['count']
+    assert num_masks == 2 * num_imgs, 'DB has wk, rbk masks: 2 * num_img'
+    
+    num_datasets = db.get(
+        Table('dataset').select(fn.Count('*')), *db_parsed
+    )[0]['count']
+    assert num_datasets == 0, 'DB has no dataset now.'
